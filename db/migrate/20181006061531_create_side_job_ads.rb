a49cc73a21dccc5e6511db40540d726ccba0fdb4
原田悠佑 <adsoofmelk1327@gmail.com>
2018-10-06 15:47:02 +0900

    model side_job_adを作成しマイグレーション
    string:name
    string:introduction
    カラムを追加

--- db/migrate/20181006061531_create_side_job_ads.rb
+++ db/migrate/20181006061531_create_side_job_ads.rb
@@ -1,7 +1,8 @@
 class CreateSideJobAds < ActiveRecord::Migration[5.2]
   def change
     create_table :side_job_ads do |t|
-
+      t.string :name
+      t.string :introduction
       t.timestamps
     end
   end
