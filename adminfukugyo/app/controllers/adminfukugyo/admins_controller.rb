23b1eae251f2b3d47f5b7c54e599acf4414d36c2
原田悠佑 <adsoofmelk1327@gmail.com>
2018-10-06 15:44:59 +0900

    engine admin側でmodelの値が参照できるかテスト

--- adminfukugyo/app/controllers/adminfukugyo/admins_controller.rb
+++ adminfukugyo/app/controllers/adminfukugyo/admins_controller.rb
@@ -3,6 +3,7 @@ require_dependency "adminfukugyo/application_controller"
 module Adminfukugyo
   class AdminsController < ApplicationController
     def index
+      @side_job_ads = SideJobAd.all
     end
   end
 end
